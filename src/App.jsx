759ee0d8bb9317d5823ab754814dcd44be0d8c43
Mansur <hmmansur050@gmail.com>
2025-05-07 12:39:55 +0600

    mansur

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-
+import { Nav,Navbar, Container,NavDropdown } from "react-bootstrap"
+import two from "./assets/two.png"
 function App() {
 
 
   return (
     <>
-   <h1>Mansur</h1>
+      <Navbar expand="lg" className="mansur">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={two} alt="LOGO (1)" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mx-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Services</Nav.Link>
+            <Nav.Link href="#link">About me</Nav.Link>
+            <Nav.Link href="#link">Portfolio</Nav.Link>
+            <Nav.Link href="#link">Contact me</Nav.Link>
+          </Nav>
+          <div className="con_btn">
+            <button>Hire Me</button>
+          </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   )
 }
